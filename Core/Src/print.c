fd37181ee8e6dff10d8a1215b5109f8a517e95c7
ManishGru <gurumanish2015@gmail.com>
2022-05-07 23:56:49 +0545

    Creating the repo for i2c communication between mpu6050 and stm32f407

---
+++ Core/Src/print.c
@@ -0,0 +1,25 @@
+/*
+ * print.c
+ *
+ *  Created on: May 7, 2022
+ *      Author: manishgru
+ */
+
+#include "stm32f4xx_hal.h"
+
+
+int ITM_SendString(char *data, int len)
+{
+        int i = 0;
+        for (; i < len; ++i)
+        {
+                ITM_SendChar(data[i]);
+        }
+        return i;
+}
+
+int _write(int file, char *data, int len)
+{
+        int sent = ITM_SendString(data, len);
+        return sent;
+}
